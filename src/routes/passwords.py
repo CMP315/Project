0f8a065a2aa046129219b31efd54213a94613483
Liam Townsley <contact@liamtownsley.me>
2024-02-28 19:56:54 +0000

    feat: implement update & delete route to user vault passwords

--- src/routes/passwords.py
+++ src/routes/passwords.py
@@ -36,6 +36,7 @@ def passwords_post(user_id:str):
     site_name:str = data.get('site_name', None) # Optional
     notes:str = data.get('notes', None) # Optional
     created_at:datetime = datetime.now()
+    last_edited_at = None
     
     try:
         user = users_collection.find_one({ '_id': ObjectId(user_id) })
@@ -48,6 +49,7 @@ def passwords_post(user_id:str):
         "username": username,
         "password": encrypted_password,
         "created_at": created_at,
+        "last_edited_at": last_edited_at,
         "site_name": site_name,
         "notes": notes
     })
@@ -61,15 +63,17 @@ def passwords_post(user_id:str):
         "password": encrypted_password,
         "site_name": site_name,
         "notes": notes,
-        "created_at": created_at
+        "created_at": created_at,
+        "last_edited_at": last_edited_at
     }), 200
     
 @app.get(PATH + "<user_id>/<site_id>")
 def passwords_get(user_id:str, site_id:str):
     password = passwords_collection.find_one({ "user_id": user_id, "_id": ObjectId(site_id) })
-    if not password: return jsonify({ "status": False, "message": "Missing Password." }), 404
-    
-    return jsonify({ "password": decrypt_password(user_id, password['password'])})
+    if not password: return jsonify({ "status": False, "message": "Password not found." }), 404
+    password['_id'] = str(password['_id'])
+    password['password'] = decrypt_password(user_id, password['password'])
+    return jsonify(password)
 
 @app.get(PATH + "<user_id>")
 def accounts_get(user_id:str):
@@ -83,3 +87,41 @@ def accounts_get(user_id:str):
         users_list.append(user)
     
     return jsonify(users_list)
+
+@app.patch(PATH + "<user_id>/<site_id>")
+def passwords_patch(user_id:str, site_id:str):
+    db_password = passwords_collection.find_one({ "user_id": user_id, "_id": ObjectId(site_id) })
+    if not db_password: return jsonify({ "status": False, "message": "Password not found." }), 404
+    
+    data = request.get_json()
+    username:str = data.get('username', None)
+    if username: db_password['username'] = username
+    
+    password:str = data.get('password', None)
+    if password:
+        encrypted_password = encrypt_password(user_id, password)
+        if not encrypted_password: return jsonify({ "status": False, "message": "Issue encrypting password."}), 500
+        db_password['password'] = encrypted_password
+    
+    site_name:str = data.get('site_name', None)
+    if site_name: db_password['site_name'] = site_name
+    
+    notes:str = data.get('notes', None)
+    if notes: db_password['notes'] = notes
+    
+    db_password['last_edited_at'] = datetime.now()
+    response = passwords_collection.replace_one({ "_id": ObjectId(site_id)}, db_password, True)
+    # if not response.acknowledged or response.modified_count == 0: return jsonify({ "status": False, "message": "Not saved to MongoDB"}), 500
+    # return jsonify(db_password)
+    db_password['_id'] = str(db_password['_id'])
+    if response.acknowledged and response.matched_count > 0:
+        return jsonify(db_password), 200
+    else:
+        return jsonify({ "status": False, "message": "Not saved to MongoDB"}), 500
+    
+@app.delete(PATH + "<user_id>/<site_id>")
+def passwords_delete(user_id:str, site_id:str):
+    response = passwords_collection.delete_one({ "user_id": user_id, "_id": ObjectId(site_id) })
+    if not response.acknowledged: return jsonify({ "status": False, "message": "Request not acknowledged by MongoDB." }), 500
+    if not response.deleted_count > 0: return jsonify({ "status": False, "message": "No documents deleted." }), 401
+    return jsonify({ "status": True })
